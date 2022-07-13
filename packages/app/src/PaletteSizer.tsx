8bc50df7ec7b09b7ba44da73678f0d322533d0b0
ivyroot <nonagoncup@gmail.com>
2022-07-13 13:45:29 -0700

    fixup

--- packages/app/src/PaletteSizer.tsx
+++ packages/app/src/PaletteSizer.tsx
@@ -1,17 +1,10 @@
 import React, { useState } from "react";
 
-function range(start, end) {
-  const length = end - start;
-  return Array.from({ length }, (_, i) => start + i);
-}
-
 export const PaletteSizer = (props) => {
   const [width, setWidth] = useState(8);
   const [height, setHeight] = useState(8);
   const MyRows = [];
   for (let i = 0; i < height; i++) {
-    // note: we are adding a key prop here to allow react to uniquely identify each
-    // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
     const myCols = [];
     for (let i2 = 0; i2 < width; i2++) {
       myCols.push(<span>-0-</span>);
