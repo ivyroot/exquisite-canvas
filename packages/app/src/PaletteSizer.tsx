c2fcad9a65e4075a974a04abc1237a46723be59c
ivyroot <nonagoncup@gmail.com>
2022-07-13 15:23:32 -0700

    initial working state for pixels on canvas

--- packages/app/src/PaletteSizer.tsx
+++ packages/app/src/PaletteSizer.tsx
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
 
+const pixelKey = (x, y) => {
+  return `${x}||${y}`;
+};
+
 export const PaletteSizer = (props) => {
   const [width, setWidth] = useState(8);
   const [height, setHeight] = useState(8);
+  const [pixels, setPixels] = useState({});
+
+  const didClickPix = (x, y) => {
+    const keyName = pixelKey(x, y);
+    console.log(`HEY YEAH E CLICKED ${keyName}`);
+    const ChangeSet = {};
+    ChangeSet[keyName] = pixels[keyName] == "ON" ? "" : "ON";
+    setPixels({ ...pixels, ...ChangeSet });
+  };
+
   const MyRows = [];
   for (let i = 0; i < height; i++) {
     const myCols = [];
     for (let i2 = 0; i2 < width; i2++) {
-      myCols.push(<span>-0-</span>);
+      const displayVal = `--0--${
+        pixels[pixelKey(i, i2)] ? pixels[pixelKey(i, i2)] : ""
+      }`;
+      myCols.push(
+        <div onClick={(event) => didClickPix(i, i2)}> {displayVal} </div>
+      );
     }
     MyRows.push(<li key={i}>{myCols}</li>);
   }
