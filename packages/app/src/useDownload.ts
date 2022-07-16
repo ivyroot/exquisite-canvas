9716ad80ac6d5327b23a765d3a30051e2db04976
ivyroot <nonagoncup@gmail.com>
2022-07-15 21:31:28 -0700

    initial working download of canvas contents as EG file

--- packages/app/src/useDownload.ts
+++ packages/app/src/useDownload.ts
@@ -1,6 +1,19 @@
 import { Pixel, PixelColor, PixelMap } from "./xgfx/api";
 import { ExquisiteBitmapHeader, PixelBuffer } from "./xgfx/ll_api";
 
+function hexStringToByte(str) {
+  if (!str) {
+    return new Uint8Array();
+  }
+
+  const a = [];
+  for (let i = 0, len = str.length; i < len; i += 2) {
+    a.push(parseInt(str.substr(i, 2), 16));
+  }
+
+  return new Uint8Array(a);
+}
+
 export function useDownload(header, palette, pixels) {
   console.log(`DOWNLOADING THE PIXELS`);
   const pb = new PixelBuffer(header, palette);
@@ -9,4 +22,12 @@ export function useDownload(header, palette, pixels) {
   });
   const data = pb.getPixelBuffer();
   console.log(`GENERATED FILE BINARY: ${data}`);
+  const bytes = hexStringToByte(data);
+  const timestamp = new Date().getTime();
+  const filename = `exquisit-gfx-file-${timestamp}.xgfx`;
+  const blob = new Blob(bytes);
+  const link = document.createElement("a");
+  link.href = window.URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
 }
