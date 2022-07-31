f50a557acdbbe72e367e063d93809e67d793ae6f
ivyroot <nonagoncup@gmail.com>
2022-07-30 22:58:04 -0700

    switch to merged logo

--- packages/app/src/CanvasLogo.tsx
+++ packages/app/src/CanvasLogo.tsx
@@ -1,131 +1,110 @@
 export const CanvasLogo = (width: string) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 140" width="122px">
+    <svg
+      id="Layer_1"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="122px"
+      viewBox="0 0 250 140"
+    >
       <defs>
-        <style>.cls-1&#123;fill:#0274fe;&#125;</style>
+        <style>.cls-1 &#123; fill:#0274fe; &#125;</style>
       </defs>
-      <g id="Layer_2" data-name="Layer 2">
-        <g id="Layer_1-2" data-name="Layer 1">
-          <rect className="cls-1" width="10" height="10" />
-          <rect className="cls-1" x="10" width="10" height="10" />
-          <rect className="cls-1" x="20" width="10" height="10" />
-          <rect className="cls-1" x="30" width="10" height="10" />
-          <rect className="cls-1" x="40" width="10" height="10" />
-          <rect className="cls-1" x="50" width="10" height="10" />
-          <rect className="cls-1" x="60" width="10" height="10" />
-          <rect className="cls-1" y="10" width="10" height="10" />
-          <rect className="cls-1" x="20" y="10" width="10" height="10" />
-          <rect className="cls-1" x="30" y="10" width="10" height="10" />
-          <rect className="cls-1" x="40" y="10" width="10" height="10" />
-          <rect className="cls-1" x="60" y="10" width="10" height="10" />
-          <rect className="cls-1" x="90" y="10" width="10" height="10" />
-          <rect className="cls-1" x="100" y="10" width="10" height="10" />
-          <rect className="cls-1" x="110" y="10" width="10" height="10" />
-          <rect className="cls-1" x="150" y="10" width="10" height="10" />
-          <rect className="cls-1" x="160" y="10" width="10" height="10" />
-          <rect className="cls-1" x="170" y="10" width="10" height="10" />
-          <rect className="cls-1" x="180" y="10" width="10" height="10" />
-          <rect className="cls-1" x="200" y="10" width="10" height="10" />
-          <rect className="cls-1" x="210" y="10" width="10" height="10" />
-          <rect className="cls-1" x="220" y="10" width="10" height="10" />
-          <rect className="cls-1" x="230" y="10" width="10" height="10" />
-          <rect className="cls-1" x="240" y="10" width="10" height="10" />
-          <rect className="cls-1" y="20" width="10" height="10" />
-          <rect className="cls-1" x="10" y="20" width="10" height="10" />
-          <rect className="cls-1" x="30" y="20" width="10" height="10" />
-          <rect className="cls-1" x="50" y="20" width="10" height="10" />
-          <rect className="cls-1" x="60" y="20" width="10" height="10" />
-          <rect className="cls-1" x="80" y="20" width="10" height="10" />
-          <rect className="cls-1" x="120" y="20" width="10" height="10" />
-          <rect className="cls-1" x="140" y="20" width="10" height="10" />
-          <rect className="cls-1" x="220" y="20" width="10" height="10" />
-          <rect className="cls-1" y="30" width="10" height="10" />
-          <rect className="cls-1" x="10" y="30" width="10" height="10" />
-          <rect className="cls-1" x="20" y="30" width="10" height="10" />
-          <rect className="cls-1" x="40" y="30" width="10" height="10" />
-          <rect className="cls-1" x="50" y="30" width="10" height="10" />
-          <rect className="cls-1" x="60" y="30" width="10" height="10" />
-          <rect className="cls-1" x="80" y="30" width="10" height="10" />
-          <rect className="cls-1" x="120" y="30" width="10" height="10" />
-          <rect className="cls-1" x="150" y="30" width="10" height="10" />
-          <rect className="cls-1" x="160" y="30" width="10" height="10" />
-          <rect className="cls-1" x="170" y="30" width="10" height="10" />
-          <rect className="cls-1" x="210" y="30" width="10" height="10" />
-          <rect className="cls-1" y="40" width="10" height="10" />
-          <rect className="cls-1" x="10" y="40" width="10" height="10" />
-          <rect className="cls-1" x="30" y="40" width="10" height="10" />
-          <rect className="cls-1" x="50" y="40" width="10" height="10" />
-          <rect className="cls-1" x="60" y="40" width="10" height="10" />
-          <rect className="cls-1" x="80" y="40" width="10" height="10" />
-          <rect className="cls-1" x="110" y="40" width="10" height="10" />
-          <rect className="cls-1" x="180" y="40" width="10" height="10" />
-          <rect className="cls-1" x="220" y="40" width="10" height="10" />
-          <rect className="cls-1" y="50" width="10" height="10" />
-          <rect className="cls-1" x="20" y="50" width="10" height="10" />
-          <rect className="cls-1" x="30" y="50" width="10" height="10" />
-          <rect className="cls-1" x="40" y="50" width="10" height="10" />
-          <rect className="cls-1" x="60" y="50" width="10" height="10" />
-          <rect className="cls-1" x="90" y="50" width="10" height="10" />
-          <rect className="cls-1" x="100" y="50" width="10" height="10" />
-          <rect className="cls-1" x="120" y="50" width="10" height="10" />
-          <rect className="cls-1" x="140" y="50" width="10" height="10" />
-          <rect className="cls-1" x="150" y="50" width="10" height="10" />
-          <rect className="cls-1" x="160" y="50" width="10" height="10" />
-          <rect className="cls-1" x="170" y="50" width="10" height="10" />
-          <rect className="cls-1" x="210" y="50" width="10" height="10" />
-          <rect className="cls-1" y="60" width="10" height="10" />
-          <rect className="cls-1" x="10" y="60" width="10" height="10" />
-          <rect className="cls-1" x="20" y="60" width="10" height="10" />
-          <rect className="cls-1" x="30" y="60" width="10" height="10" />
-          <rect className="cls-1" x="40" y="60" width="10" height="10" />
-          <rect className="cls-1" x="50" y="60" width="10" height="10" />
-          <rect className="cls-1" x="60" y="60" width="10" height="10" />
-          <rect className="cls-1" x="20" y="90" width="10" height="10" />
-          <rect className="cls-1" x="30" y="90" width="10" height="10" />
-          <rect className="cls-1" x="40" y="90" width="10" height="10" />
-          <rect className="cls-1" x="70" y="90" width="10" height="10" />
-          <rect className="cls-1" x="110" y="90" width="10" height="10" />
-          <rect className="cls-1" x="130" y="90" width="10" height="10" />
-          <rect className="cls-1" x="170" y="90" width="10" height="10" />
-          <rect className="cls-1" x="200" y="90" width="10" height="10" />
-          <rect className="cls-1" x="210" y="90" width="10" height="10" />
-          <rect className="cls-1" x="220" y="90" width="10" height="10" />
-          <rect className="cls-1" x="230" y="90" width="10" height="10" />
-          <rect className="cls-1" x="10" y="100" width="10" height="10" />
-          <rect className="cls-1" x="50" y="100" width="10" height="10" />
-          <rect className="cls-1" x="80" y="100" width="10" height="10" />
-          <rect className="cls-1" x="110" y="100" width="10" height="10" />
-          <rect className="cls-1" x="130" y="100" width="10" height="10" />
-          <rect className="cls-1" x="170" y="100" width="10" height="10" />
-          <rect className="cls-1" x="190" y="100" width="10" height="10" />
-          <rect className="cls-1" x="10" y="110" width="10" height="10" />
-          <rect className="cls-1" x="70" y="110" width="10" height="10" />
-          <rect className="cls-1" x="90" y="110" width="10" height="10" />
-          <rect className="cls-1" x="110" y="110" width="10" height="10" />
-          <rect className="cls-1" x="130" y="110" width="10" height="10" />
-          <rect className="cls-1" x="170" y="110" width="10" height="10" />
-          <rect className="cls-1" x="200" y="110" width="10" height="10" />
-          <rect className="cls-1" x="210" y="110" width="10" height="10" />
-          <rect className="cls-1" x="220" y="110" width="10" height="10" />
-          <rect className="cls-1" x="10" y="120" width="10" height="10" />
-          <rect className="cls-1" x="50" y="120" width="10" height="10" />
-          <rect className="cls-1" x="70" y="120" width="10" height="10" />
-          <rect className="cls-1" x="100" y="120" width="10" height="10" />
-          <rect className="cls-1" x="140" y="120" width="10" height="10" />
-          <rect className="cls-1" x="160" y="120" width="10" height="10" />
-          <rect className="cls-1" x="230" y="120" width="10" height="10" />
-          <rect className="cls-1" x="20" y="130" width="10" height="10" />
-          <rect className="cls-1" x="30" y="130" width="10" height="10" />
-          <rect className="cls-1" x="40" y="130" width="10" height="10" />
-          <rect className="cls-1" x="70" y="130" width="10" height="10" />
-          <rect className="cls-1" x="110" y="130" width="10" height="10" />
-          <rect className="cls-1" x="150" y="130" width="10" height="10" />
-          <rect className="cls-1" x="190" y="130" width="10" height="10" />
-          <rect className="cls-1" x="200" y="130" width="10" height="10" />
-          <rect className="cls-1" x="210" y="130" width="10" height="10" />
-          <rect className="cls-1" x="220" y="130" width="10" height="10" />
-        </g>
-      </g>
+      <path
+        className="cls-1"
+        d="M50,0H0V70H70V0H50ZM60,20H50V30H40V40H50V50H60V60H50V50H40V40H30V50H20V60H10V50H20V40H30V30H20V20H10V10H20V20H30V30H40V20H50V10H60Z"
+      />
+      <polygon
+        className="cls-1"
+        points="110 20 120 20 120 10 110 10 100 10 90 10 90 20 100 20 110 20"
+      />
+      <polygon
+        className="cls-1"
+        points="170 10 160 10 150 10 150 20 160 20 170 20 180 20 190 20 190 10 180 10 170 10"
+      />
+      <polygon
+        className="cls-1"
+        points="240 10 230 10 220 10 210 10 200 10 200 20 210 20 220 20 220 30 230 30 230 20 240 20 250 20 250 10 240 10"
+      />
+      <polygon
+        className="cls-1"
+        points="120 30 120 40 130 40 130 30 130 20 120 20 120 30"
+      />
+      <rect className="cls-1" x="140" y="20" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="90 30 90 20 80 20 80 30 80 40 80 50 90 50 90 40 90 30"
+      />
+      <polygon
+        className="cls-1"
+        points="160 40 170 40 180 40 180 30 170 30 160 30 150 30 150 40 160 40"
+      />
+      <rect className="cls-1" x="210" y="30" width="10" height="10" />
+      <rect className="cls-1" x="110" y="40" width="10" height="10" />
+      <rect className="cls-1" x="180" y="40" width="10" height="10" />
+      <rect className="cls-1" x="220" y="40" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="90 50 90 60 100 60 110 60 110 50 100 50 90 50"
+      />
+      <rect className="cls-1" x="120" y="50" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="160 50 150 50 140 50 140 60 150 60 160 60 170 60 180 60 180 50 170 50 160 50"
+      />
+      <rect className="cls-1" x="210" y="50" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="40 90 30 90 20 90 20 100 30 100 40 100 50 100 50 90 40 90"
+      />
+      <rect className="cls-1" x="70" y="90" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="110 100 110 110 110 120 120 120 120 110 120 100 120 90 110 90 110 100"
+      />
+      <polygon
+        className="cls-1"
+        points="170 100 170 110 170 120 180 120 180 110 180 100 180 90 170 90 170 100"
+      />
+      <polygon
+        className="cls-1"
+        points="220 100 230 100 240 100 240 90 230 90 220 90 210 90 200 90 200 100 210 100 220 100"
+      />
+      <polygon
+        className="cls-1"
+        points="10 110 10 120 10 130 20 130 20 120 20 110 20 100 10 100 10 110"
+      />
+      <rect className="cls-1" x="50" y="100" width="10" height="10" />
+      <rect className="cls-1" x="80" y="100" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="140 100 140 90 130 90 130 100 130 110 130 120 140 120 140 110 140 100"
+      />
+      <rect className="cls-1" x="190" y="100" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="70 120 70 130 70 140 80 140 80 130 80 120 80 110 70 110 70 120"
+      />
+      <rect className="cls-1" x="90" y="110" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="210 120 220 120 230 120 230 110 220 110 210 110 200 110 200 120 210 120"
+      />
+      <rect className="cls-1" x="50" y="120" width="10" height="10" />
+      <rect className="cls-1" x="100" y="120" width="10" height="10" />
+      <rect className="cls-1" x="140" y="120" width="10" height="10" />
+      <rect className="cls-1" x="160" y="120" width="10" height="10" />
+      <rect className="cls-1" x="230" y="120" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="30 140 40 140 50 140 50 130 40 130 30 130 20 130 20 140 30 140"
+      />
+      <rect className="cls-1" x="110" y="130" width="10" height="10" />
+      <rect className="cls-1" x="150" y="130" width="10" height="10" />
+      <polygon
+        className="cls-1"
+        points="210 130 200 130 190 130 190 140 200 140 210 140 220 140 230 140 230 130 220 130 210 130"
+      />
     </svg>
   );
 };
