86796e6fc296d8275d3fb5f8b401c8cefe605e16
ivyroot <nonagoncup@gmail.com>
2022-08-17 10:39:04 -0700

    add icons for social links in footer

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -13,7 +13,31 @@ const HomePage: NextPage = () => {
   return (
     <div>
       <DemoCanvas />
-      <div className="h-36 bg-white"></div>
+      <div className="h-36 bg-white">
+        <div className="flex justify-center mt-8">
+          <a
+            href="https://twitter.com/ivyrootcode"
+            className="mx-4"
+            target="_none"
+          >
+            <img src="/twitter-logo-blue.png" width="60px"></img>
+          </a>
+          <a
+            href="https://github.com/ivyroot/exquisite-canvas"
+            className="mx-4"
+            target="_none"
+          >
+            <img src="/github-3.svg" width="60px"></img>
+          </a>
+          <a href="https://twitter.com/xqstgfx" className="mx-4" target="_none">
+            <img
+              src="/xqst-gfx.jpeg"
+              className="rounded-full"
+              width="60px"
+            ></img>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
