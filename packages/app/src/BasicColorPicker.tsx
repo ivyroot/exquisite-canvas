8bf798185c467365b9f25d01d085b008b7afe389
ivyroot <nonagoncup@gmail.com>
2022-08-16 22:03:35 -0700

    simplify code

--- packages/app/src/BasicColorPicker.tsx
+++ packages/app/src/BasicColorPicker.tsx
@@ -6,17 +6,13 @@ import { CanvasStore } from "./xqcanvas/CanvasInterfaces";
 export const BasicColorPicker = (props: { canvas: CanvasStore }) => {
   const canvas = props.canvas;
 
-  const currPaletteItemColor = () => {
-    return canvas.getPaletteItemColor(canvas.currPaletteItem);
-  };
-
   const setCurrPaletteItemColor = (color: string) => {
     canvas.setPaletteItem(canvas.currPaletteItem, color);
   };
 
   return (
     <HexColorPicker
-      color={currPaletteItemColor()}
+      color={canvas.getPaletteItemColor(canvas.currPaletteItem)}
       onChange={setCurrPaletteItemColor}
     />
   );
