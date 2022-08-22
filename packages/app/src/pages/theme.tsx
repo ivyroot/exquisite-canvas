0a8a3b99d11bb5a71e9901f03f48f40d9225cb5f
ivyroot <nonagoncup@gmail.com>
2022-08-21 22:33:25 -0700

    code formatting

--- packages/app/src/pages/theme.tsx
+++ packages/app/src/pages/theme.tsx
@@ -1,42 +1,42 @@
 import type { NextPage } from "next";
-import { CanvasSkin } from "../CanvasSkin";
+
 import { CanvasLogo } from "../CanvasLogo";
+import { CanvasSkin } from "../CanvasSkin";
 
 const ThemePage: NextPage = () => {
-
-  const itemNames = [ 
-    "dropper", 
-    "dropper-active", 
-    "move-up", 
-    "move-down", 
-    "move-left", 
+  const itemNames = [
+    "dropper",
+    "dropper-active",
+    "move-up",
+    "move-down",
+    "move-left",
     "move-right",
   ];
 
-  const themeItems = itemNames.map( i => {
+  const themeItems = itemNames.map((i) => {
     return (
-        <div class='m-8'>
-          <div class='my-4'>{i}:</div>
-          <CanvasSkin item={i}></CanvasSkin>
-        </div>
+      <div className="m-8">
+        <div className="my-4">{i}:</div>
+        <CanvasSkin item={i}></CanvasSkin>
+      </div>
     );
   });
 
   return (
-    <div class='bg-slate-800 text-blue-600'>
+    <div className="bg-slate-800 text-blue-600">
       <div id="headerNav" className="flex flex-wrap justify-left">
         <div className="ml-6 my-2 w-20 sm:w-28">
-          <a href='/'>
+          <a href="/">
             <CanvasLogo></CanvasLogo>
           </a>
         </div>
       </div>
-      <h1 class='text-center text-2xl py-12'>
+      <h1 className="text-center text-2xl py-12">
         Exquisite Canvas Theme: Default
       </h1>
-      <div class='flex flex-wrap items-center justify-center'>
+      <div className="flex flex-wrap items-center justify-center">
         <div className="w-20 sm:w-28">
-          <div class='my-4'>logo:</div>
+          <div className="my-4">logo:</div>
           <CanvasLogo></CanvasLogo>
         </div>
         {themeItems}
@@ -44,16 +44,14 @@ const ThemePage: NextPage = () => {
       <div className="h-screen bg-slate-800">
         <div className="flex justify-center mt-8">
           By
-            <a
-              href="https://twitter.com/michaelbelong"
-              className="mx-1"
-              target="_none"
-            >
-              @michaelbelong
-              aka Mendicant Bias
-            </a>
-          </div>
-
+          <a
+            href="https://twitter.com/michaelbelong"
+            className="mx-1"
+            target="_none"
+          >
+            @michaelbelong aka Mendicant Bias
+          </a>
+        </div>
       </div>
     </div>
   );
